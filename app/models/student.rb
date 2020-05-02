70d5b340f7718606f4bd9f35b7ebcef125432e5f
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-02 22:43:36 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+ def self.search(query)
+    if query
+      where("name LIKE ?", "%#{query}%")
+    else
+      self.all
+    end
+  end
+
 end
